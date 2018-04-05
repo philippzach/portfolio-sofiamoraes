6d25467172b64e73ff5bf1ffefe68c0e0f6f696d
philippzach <mail@philippzach.com>
2018-04-05 08:04:19 -1000

    Added Typist Package

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './aboutme.module.scss';
 import tachyons from 'tachyons';
 import Profilepic from './aboutsm.jpg';
+import Typist from 'react-typist';
 
 const Aboutme = () => (
   <div>
@@ -13,6 +14,7 @@ const Aboutme = () => (
         <div className="fl w-100 w-50-ns pa2 measure-narrow">
           <div className={styles.abouttext}>
             <h2>About Me</h2>
+            { /* }<Typist cursor= {{show: false,  }}>{*/}
             <p>is simply dummy text of the printing and typesetting industry.</p>
             <p>
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took
@@ -22,11 +24,13 @@ const Aboutme = () => (
               it to make a type specimen book. It has survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged.
             </p>
+            { /* }</Typist>{*/}
           </div>
         </div>
       </div>
     </div>
   </div>
+  
 );
 
 export default Aboutme;
