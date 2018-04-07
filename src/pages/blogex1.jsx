a43834b516d447884db89f7e8513ef84820af349
philippzach <mail@philippzach.com>
2018-04-06 16:23:29 -1000

    Blog Template 1

---
+++ src/pages/blogex1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Container from '../components/Container/Container';
+import Footer from '../components/Footer/Footer';
+import config from '../../config/SiteConfig';
+import Blog1 from '../components/Blog1/Blog1';
+
+const Blog = () => (
+  <div>
+    <Helmet title={`Random Blog Title | ${config.siteTitle}`} />
+    <Blog1 />
+    <Footer />
+  </div>
+);
+
+export default Blog;
