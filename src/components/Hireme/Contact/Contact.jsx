20dfa694fdd8269c8bc5accb92d92a65fdc91925
philippzach <mail@philippzach.com>
2018-04-09 11:47:19 -1000

    Hire me before Stripe

---
+++ src/components/Hireme/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => (
+  <div className="mw7 bl ml5-ns" style={{ marginBottom: '5rem' }}>
+    <p className="pl4 pt5">
+      Do you have a bigger project in mind? Like Web or App Design, Branding or Advertisement? Email me about your
+      project >{' '}
+      <a href="mailto:hi@sofiamoraes.com" class="b">
+        hi@sofiamoraes.com
+      </a>
+    </p>
+  </div>
+);
+
+export default Contact;
