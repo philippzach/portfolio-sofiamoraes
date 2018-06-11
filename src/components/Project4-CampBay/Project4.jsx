15b345f0941f2835710546ce0cf2f4a93c54e9aa
philippzach <mail@philippzach.com>
2018-06-11 18:43:31 +0200

    Camp Bay almost ready

--- src/components/Project4-CampBay/Project4.jsx
+++ src/components/Project4-CampBay/Project4.jsx
@@ -5,10 +5,12 @@ import Random from '../ProjectsComponents/BrowseProjects';
 import styles from '../ProjectsComponents/Project.module.scss';
 import syle from './Project4.module.scss';
 import Button from '../Buttons/Button1/Button1';
-import Logos from '../../../Design Elements/Project2-CampBay/Palm Icon-01.svg';
+import Logos from '../../../Design Elements/Project2-CampBay/logo-icons.svg';
+import Picture from '../../../Design Elements/Project2-CampBay/cblphoto-sm.png';
+import Group from '../../../Design Elements/Project2-CampBay/Group-5.png';
+
 // import Carved from '../../../Design Elements/Project2-CampBay/';
-import Blue from '../../../Design Elements/Project2-CampBay/bt-01.svg';
-import Grey from '../../../Design Elements/Project2-CampBay/gd-01.svg';
+import Palet from '../../../Design Elements/Project2-CampBay/palet-01.svg';
 import Sketch1 from '../../../Design Elements/Project2-CampBay/1-01.svg';
 import Sketch2 from '../../../Design Elements/Project2-CampBay/2-01.svg';
 import Sketch3 from '../../../Design Elements/Project2-CampBay/3-01.svg';
@@ -20,12 +22,12 @@ const Project4 = () => (
   <div>
     <ProjectTop heading="Camp Bay Lodge" subHeading="A mindful experience" pic={HeaderImage} altText="" />
     <div className={styles.container}>
-      <section className={styles.content}>
-        <div className={styles.left}>
+      <section className={styles.content} style={{marginTop: '4em', marginBottom: '4em'}}>
+        <div className={styles.divleft}>
           <h2 className={styles.heading}>What is actually</h2>
           <h3>Camp Bay Lodge?</h3>
         </div>
-        <div className={styles.right}>
+        <div className={styles.divright}>
           <p className={styles.text}>
             It is a hotel located in the uncrowded East end of Roatan, totally removed from the regular tourism hustle
             surrounded by exuberant nature.
@@ -34,9 +36,9 @@ const Project4 = () => (
         </div>
       </section>
     </div>
-    <div className={styles.container} style={{ backgroundColor: 'darkgrey' }}>
+    <div className={styles.container} style={{ backgroundColor: 'rgb(50,51,52)' }}>
       <section className={styles.content}>
-        <div className={styles.left} style={{ padding: '10% 2%' }}>
+        <div className={styles.divleft} style={{ padding: '10% 2%' }}>
           <h2 className={styles.heading} style={{ color: 'white' }}>
             Building the Identity
           </h2>
@@ -47,24 +49,24 @@ const Project4 = () => (
             with the moment through yoga, meditation or any sports activity.
           </p>
         </div>
-        <div className={styles.right}>
+        <div className={styles.divright}>
           <img className={styles.acailogo} src={Logos} alt="El Acai Europe Logo Brand Identity" />
         </div>
       </section>
     </div>
     <div className={styles.container}>
-      <section className="dt mw6 center pv4 pv5-m pv6-ns">
-        <div className="dtc v-mid">
-          <img src={Logos} alt="Turquoise Media Fonts Scheme Sofia Moraes copyright" className="mw5" />
+      <section className={styles.content} style={{marginTop: '6em', marginBottom: '6em'}}>
+        <div className={styles.divleft}>
+          <img src={Picture} alt="Turquoise Media Fonts Scheme Sofia Moraes copyright" />
         </div>
-        <div className="dtc v-mid pl3">
-          <h3>
+        <div className={styles.divright}>
+          <h3 className={syle.alignvertical}>
             The final logo was also carved in wood by local artists, giving an original touch to each space of the place
           </h3>
         </div>
       </section>
-      <section className={styles.content}>
-        <div className={styles.left}>
+      
+        <div className={styles.floatleft}>
           <h2 className={styles.heading}>Color Palette</h2>
           <h3>Inspired by nature</h3>
           <p className={styles.text}>
@@ -73,73 +75,82 @@ const Project4 = () => (
             give a professional and reliable style.
           </p>
         </div>
-        <div className={styles.right}>
-          <img src={Blue} alt="" />
-          <img src={Grey} alt="" />
+        <div>
+        <img src={Palet} alt="" style={{ width: '100%', maxWidth: 'none' }}/>
         </div>
-      </section>
+   
       <div className="tc">
-        <h2 className={styles.heading}>Developing the logo & base shape</h2>
-        <h3>
+        <h2 className={styles.heading} style={{marginTop: '4em', marginBottom:'2em'}}>Developing the logo & base shape</h2>
+        <h3 className={styles.h3text} style={{marginBottom:'4em'}}>
           The main idea for the realisation of the base shape was representing the universal law of: “ as it is above so
           below” referring to every thought that we place in our mind will shape our reality.
         </h3>
       </div>
-      <div className="tc">
-        <img src={Sketch1} alt="" />
-        <img src={Sketch2} alt="" />
-        <img src={Sketch3} alt="" />
+      <div className={syle.flex}>
+      <div>
+        <img className={syle.icons} src={Sketch1} alt="" style={{marginTop: '5px', width: '25em'}} />
+        </div>
+        <div>
+        <img className={syle.icons} src={Sketch2} alt="" style={{width: '25em'}}/>
+        </div>
+        <div>
+        <img className={syle.icons} src={Sketch3} alt="" style={{width: '25em'}}/>
+        </div>
       </div>
+      <div className={styles.containersm} style={{marginTop: '4em', marginBottom: '6em'}}>
       <div className="tc">
-        <h3>
+        <h3 className={styles.h3text}>
           This idea was reflected in a very simple way though two inverse triangles and a circle representing the whole.
           The definition was very pleasant for the client due that Camp Bay Lodge is not just an hotel but a space for
           reflection where you can find the balance in your mind and therefore in Life.
         </h3>
       </div>
+      </div>
     </div>
-    <div className={styles.container} style={{ backgroundColor: 'darkgrey' }}>
-      <h1>Placeholder</h1>
-    </div>
+    <img src={Group} alt="" />
     <div className={styles.container}>
       <div className="tc">
-        <h2 className={styles.heading}>What I love the most while designing this</h2>
-        <h3>
+        <h2 className={styles.heading} style={{marginTop: '4em'}}>What I love the most while designing this!</h2>
+        <div className={styles.containersm}>
+        <h3 className={styles.h3text} style={{marginTop: '2em', marginBottom: '6em'}}>
           This idea was reflected in a very simple way though two inverse triangles and a circle representing the whole.
           The definition was very pleasant for the client due that Camp Bay Lodge is not just an hotel but a space for
           reflection where you can find the balance in your mind and therefore in Life.
         </h3>
+        </div>
       </div>
       <section className={styles.content}>
         <div className={styles.left}>
-          <img src={Mockup1} alt="" />
+          <img style={{marginTop: '3em', padding: '3%'}} src={Mockup1} alt="" />
         </div>
         <div className={styles.right}>
-          <img src={Mockup2} alt="" />
+          <img style={{padding: '3%'}} src={Mockup2} alt="" />
         </div>
       </section>
     </div>
-    <div className={styles.container} style={{ backgroundColor: 'darkgrey' }}>
+    <div className={styles.container} style={{ backgroundColor: 'lightgrey' }}>
       <section className={styles.content}>
-        <div className={styles.left}>
-          <h2 className={styles.heading} style={{ color: 'white' }}>On-side website</h2>
-          <h3 style={{ color: 'white' }}>Merging all the elements</h3>
-          <p className={styles.text} style={{ color: 'white' }}>
+        <div className={styles.divleft} style={{marginTop: '12%'}}>
+          <h2 className={styles.heading} style={{  }}>On-side website</h2>
+          <h3 style={{  }}>Merging all the elements</h3>
+          <p className={styles.text} style={{  }}>
             My work was not limited to graphic design, but also to the production of audiovisual, as well as being part
             of the web design team.
           </p>
         </div>
-        <div className={styles.right}>
+        <div className={styles.divright}>
           <img src={Mockup2} alt="" />
         </div>
       </section>
     </div>
     <div className={styles.container}>
-      <div className="tc">
-        <h1 className="measure">
+    <div className={styles.containersm}>
+      <div className="tc" style={{marginTop: '4em', marginBottom: '6em'}}>
+        <h2 className={syle.lastwords}>
           <b>Camp Bay lodge is part of one of the most remote and original communities in the world</b> full of life,
           exotic food and surrounded by ancient special feeling.
-        </h1>
+        </h2>
+        </div>
       </div>
     </div>
     <Next link='/turquoisemedia' />
