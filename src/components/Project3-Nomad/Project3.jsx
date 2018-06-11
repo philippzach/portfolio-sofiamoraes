80e6507a492bb889738aac6698731fc76927b0fd
philippzach <mail@philippzach.com>
2018-06-11 16:17:29 +0200

    Nomad Yoga done

--- src/components/Project3-Nomad/Project3.jsx
+++ src/components/Project3-Nomad/Project3.jsx
@@ -20,48 +20,45 @@ const Project3 = () => (
   <div>
     <ProjectTop heading="Nomad Yoga" subHeading="Ready to feel good?" pic={HeaderImage} altText="" />
     <div className={styles.container}>
-      <div className={styles.floatright}>
-        <h2 className={styles.heading}>What is Nomad Yoga</h2>
+      <div className={styles.floatright} style={{textAlign: 'right', marginTop: '4em', marginBottom: '4em'}}>
+        <h2 className={styles.heading}>What is Nomad Yoga?</h2>
         <p className={styles.text}>
-          Is a App which presents a series of soulful yoga curated for daily use, designed to help people to build a
-          morning routine, connect with other yoguis, center, or simple disconnect from a busy life.
+          Is a mobile application which presents a series of soulful yoga practices curated for daily use. Designed to help people build a
+          morning routine, connect with other yogis, or simple disconnect from a busy life.
         </p>
       </div>
-      <div className={styles.horizontalline} />
-      <div className="tc">
-        <p className={styles.text}>
+      <hr className={styles.horizontalline} />
+      
+      <div className={styles.containersm} style={{marginTop: '4em', marginBottom:' 4em'}}>
+        <p className={styles.text} style={{textAlign: 'center'}}>
           The founders of Nomad Yoga expressed their desire to create an application in which people had the opportunity
           to create a habit around their practice.
         </p>
       </div>
-    </div>
+      </div>
     <div className={syle.greybackground}>
-      <img src={Phone1} />
-      <img src={Phone2} />
-      <img src={Phone3} />
+    <div>
+      <img className={syle.iphone} src={Phone1} />
+      </div>
+      <div>
+      <img className={syle.iphone} src={Phone2} />
+      </div>
+      <div>
+      <img className={syle.iphone} src={Phone3} />
+      </div>
     </div>
     <div className={styles.container}>
-      <section className={styles.content}>
-        <div className={styles.left}>
-          <h2 className={styles.heading}>Logo</h2>
-          <h3>Spraying those vibes!</h3>
-          <p className={styles.text}>Simple and minimal, allowing the brands' spirit and modern vibe shine.</p>
+      <section className={styles.content} style={{ marginTop: '6em'}}>
+        <div className={styles.divleft}>
+          <h2 className={styles.heading}>The Design</h2>
+          <h3>Giving life to the lines</h3>
+          <p className={styles.text}>I incorporate bluish tones into the desgin which habe the characteristics of expressing tranquility, relaxation & feelings of well being.</p>
         </div>
-        <div className={styles.right}>
+        <div className={styles.divright}>
           <img src={Logo} alt="Nomad Yoga Logo Sofia Moraes copyright" />
         </div>
       </section>
-      <section className="dt mw6 center pv4 pv5-m pv6-ns">
-        <div className="dtc v-mid">
-          <h3>Comparisson Logo with Tachyons Flag</h3>
-          <h2 className={styles.heading}>Spraying those vibes!</h2>
-          <p className={styles.text}>Simple and minimal, allowing the brands' spirit and modern vibe shine.</p>
-        </div>
-        <div className="dtc v-mid pl3">
-          <img src={Logo} alt="Nomad Yoga Logo Sofia Moraes copyright" className="mw5" />
-        </div>
-      </section>
-      <div className={styles.floatright}>
+      <div className={styles.floatright} style={{textAlign: 'right', marginTop: '6em'}}>
         <h2 className={styles.heading}>Goals</h2>
         <p className={styles.text}>
           Create a minimal, easy and focused practice that offers the three elements of habit: simplicity, community and
@@ -69,19 +66,27 @@ const Project3 = () => (
           commitment with the application.
         </p>
       </div>
+      <div className={syle.whitebackground}>
       <div>
-        <img src={Ring1} />
-        <img src={Ring2} />
-        <img src={Ring3} />
+        <img className={syle.circles} src={Ring1} />
+      </div>
+      <div>
+        <img className={syle.circles} src={Ring2} />
+        </div>
+      <div>
+        <img className={syle.circles} src={Ring3} />
+        </div>
       </div>
       <div className={styles.horizontalline} />
-      <div className="tc">
-        <h3>
-          The purpose of creating user flows for this application was mainly to demonstrate in a simple way the founders
-          and developers the operation of the app.
-        </h3>
+      <div className="tc" style={{marginTop: '6em'}}>
+        <div className={styles.containersm} style={{marginBottom: '4em'}}>
+          <h3 className={styles.h3text}>
+            The purpose of creating user flows for this application was mainly to demonstrate in a simple way the founders
+            and developers the operation of the app.
+          </h3>
+        </div>
         <img src={Diagram} />
-        <div className={styles.floatright}>
+        <div className={styles.floatright} style={{textAlign: 'right', marginBottom: '4em', marginTop: '4em'}}>
           <h2 className={styles.heading}>Simple UI</h2>
           <h3>Simplicity is the closest way to the heart</h3>
           <p className={styles.text}>
@@ -94,10 +99,12 @@ const Project3 = () => (
         </div>
         <img src={Mockup} style={{ width: "100%"}} />
         <div className={styles.container}>
-        <h3 className={styles.h3text}>
+        <div className={styles.containersm}>
+        <h3 className={styles.h3text} style={{textAlign: 'center', marginBottom: '4em'}}>
           The creation of iconography and miniatures was inspired by the soft tones of the main design palette, which
           resulted in a very pleasing appearance to the eye.
         </h3>
+        </div>
         </div>
         <img src={Apps} style={{ width: "100%"}} />
     <Next link='/campbaylodge' />
