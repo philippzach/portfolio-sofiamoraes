d6652991d160dfaf44b5c8b20fab0992a0da971e
philippzach <mail@philippzach.com>
2018-06-12 16:15:35 +0200

    quantum peak coaching finished

--- src/components/Project6-Quantum/Project6.jsx
+++ src/components/Project6-Quantum/Project6.jsx
@@ -3,6 +3,7 @@ import ProjectTop from '../ProjectsComponents/ProjectTop';
 import HeaderImage from '../../../Design Elements/Project4-Quantum Peak Coaching/qpq-sm.jpg';
 import Random from '../ProjectsComponents/BrowseProjects';
 import styles from '../ProjectsComponents/Project.module.scss';
+import syle from './Project6.module.scss';
 import Phone from '../../../Design Elements/Project4-Quantum Peak Coaching/iphone-sm.jpg';
 import Logo from '../../../Design Elements/Project4-Quantum Peak Coaching/l-01.svg';
 import Colors from '../../../Design Elements/Project4-Quantum Peak Coaching/cp-01.svg';
@@ -15,22 +16,22 @@ const Project6 = () => (
   <div>
     <ProjectTop heading="Quantum Peak Coaching" subHeading="Reaching your highest peaks" pic={HeaderImage} altText="" />
     <div className={styles.container}>
-      <div className={styles.floatright}>
+      <div className={styles.floatright} style={{textAlign: 'right', marginTop: '2em', marginBottom: '4em'}}>
         <h2 className={styles.heading}>Quantum Peak Coaching</h2>
-        <h3>There is no such thing as reality, only our perception of it</h3>
+        <h3 className={styles.h3text}>There is no such thing as reality,<br/> only our perception of it</h3>
       </div>
-      <section className={styles.content}>
-        <div className={styles.left}>
+      <section className={styles.content} style={{marginBottom: '4em'}}>
+        <div className={syle.divleft}>
           <h2 className={styles.heading}>
             The Client
           </h2>
           <p className={styles.text}>
-            Quantum Peak Coaching It is a Canadian company committed to support of people who consider a profound change
-            in their lives, based on values and strength of personal influence. specializes, among others, in training,
+            Quantum Peak Coaching It is a Canadian company committed to support people who consider a profound change
+            in their lives. Based on values of strength and personal influence it specializes in training,
             consulting and NLP training programs.
           </p>
         </div>
-        <div className={styles.right}>
+        <div className={syle.divright}>
           <h2 className={styles.heading}>
             The Solution
           </h2>
@@ -43,7 +44,7 @@ const Project6 = () => (
     </div>
     <img src={Phone} alt="" />
     <div className={styles.container}>
-      <div className={styles.floatright}>
+      <div className={styles.floatright} style={{marginTop: '4em', textAlign: 'right', marginBottom: '4em'}}>
         <h2 className={styles.heading}>Building the Logo</h2>
         <p className={styles.text}>
           The graphic representation of the peak was a great metaphorical link to the peak of success and happiness they
@@ -54,7 +55,7 @@ const Project6 = () => (
       <div className="tc">
         <img src={Logo} alt="" />
       </div>
-      <div>
+      <div style={{marginTop: '4em', marginBottom: '6em'}}>
         <h2 className={styles.heading}>
           Color Palette
         </h2>
@@ -63,29 +64,31 @@ const Project6 = () => (
       <div className="tc">
         <img src={Colors} alt="" />
       </div>
-      <div className={styles.floatright}>
+      <div className={styles.floatright} style={{textAlign: 'right'}}>
         <h2 className={styles.heading}>Fonts Used</h2>
-        <p className={styles.text}>A combination of sans serif which expresses decision making, clarity and balance</p>
+        <h3 className={styles.h3text}>A combination of sans serif which expresses decision making, clarity and balance</h3>
       </div>
       <div>
         <img src={Font1} alt="" />
       </div>
-      <div className="tc">
+      <div className="tc" style={{marginTop: '4em', marginBottom: '4em'}}>
         <h3>A great combination to reflect the core values of the company.</h3>
       </div>
       <div className={styles.floatright}>
         <img src={Font2} alt="" />
       </div>
       <div className={styles.horizontalline} />
+      <div className={styles.containersm} style={{marginTop: '4em', marginBottom: '6em'}}>
       <div className="tc">
-        <h3>
+        <h3 className={styles.h3text}>
           The brand visual is the final word on Quantum Peak Coaching identity: logotype, typography, color palette,
           photography, and more. It comes with detailed instructions on how to use these elements in a variety of
           contexts.
         </h3>
       </div>
+      </div>
     </div>
-    <img src={Last} alt="" />
+    <img src={Last} alt="" style={{marginBottom: '4em'}}/>
     <Next link="/mandalita" />
     <Random />
   </div>
