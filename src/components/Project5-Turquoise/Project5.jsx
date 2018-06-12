a6dafba1ee58812a17a71b1dc34bb68b6cec43ed
philippzach <mail@philippzach.com>
2018-06-12 15:50:20 +0200

    Turqoiuse Media finished

--- src/components/Project5-Turquoise/Project5.jsx
+++ src/components/Project5-Turquoise/Project5.jsx
@@ -18,13 +18,15 @@ const Project5 = () => (
   <div>
     <ProjectTop heading="Turquoise Media" subHeading="Design | Captivate | Convert" pic={HeaderImage} altText="" />
     <div className={styles.container}>
-      <div>
+    <section className={styles.content} style={{marginTop: '2em'}}>
+      <div className={styles.divleft}>
         <h2 className={styles.heading}>The Company</h2>
-        <h3>
+        <h3 className={styles.h3text}>
           Turquoise Media is an independent international creative agency that leads commerces into a digital world
         </h3>
       </div>
-      <div className={styles.floatright}>
+      </section>
+      <div className="tr" style={{marginTop: '2em', marginBottom: '6em'}}>
         <h2 className={styles.heading}>The Solution</h2>
         <p className={styles.text}>
           The founders approached me to elaborate some changes to their already established brand, of which consisted in
@@ -33,59 +35,66 @@ const Project5 = () => (
         </p>
       </div>
       <div className={styles.horizontalline} />
-      <div>
+      <section className={styles.content}>
+      <div className={styles.divleft} style={{marginTop: '6em'}}>
         <h2 className={styles.heading}>Rebranding</h2>
         <h3>The Logo</h3>
         <p className={styles.text}>
           After many iterations, I found the right graphic representation of freedom, flexibility, creativity and that
           funky style that expresses what the company is about.
         </p>
-        <div className="tc">
+        </div>
+        </section>
+        <div style={{marginTop: '4em'}} className="tc">
           <img src={Logo} />
         </div>
-        <div className="tc">
+        <div style={{marginTop: '4em'}} className="tc">
           <img src={Logo1} />
         </div>
       </div>
-    </div>
-    <section className="dt mw6 center pv4 pv5-m pv6-ns">
-      <div className="dtc v-mid">
+    
+    <section className={styles.content} style={{marginTop: '6em', marginBottom: '6em'}}>
+      <div className={syle.divleft}>
         <h2 className={styles.heading}>An updated website</h2>
         <p className={styles.text}>
           With their new site, the aim was to communicate in the simplest, fastest, and most elegant way their strengths
           as an creative agency.
         </p>
       </div>
-      <div className="dtc v-mid pl3">
-        <img src={Website} alt="Turquoise Media Webstite Mockup Sofia Moraes copyright" className="mw5" />
+      <div className={syle.divright}>
+        <img src={Website} alt="Turquoise Media Webstite Mockup Sofia Moraes copyright" style={{width: '100%'}}/>
       </div>
     </section>
     <div className={styles.container}>
       <div className={styles.horizontalline} />
-      <section className="dt mw6 center pv4 pv5-m pv6-ns">
-        <div className="dtc v-mid">
-          <img src={Colors} alt="Turquoise Media Color Scheme Sofia Moraes copyright" className="mw5" />
+      <section className={styles.content} style={{marginTop: '4em', marginBottom: '4em'}}>
+        <div className={styles.divleft}>
+          <img src={Colors} alt="Turquoise Media Color Scheme Sofia Moraes copyright" style={{width: '100%'}} />
         </div>
-        <div className="dtc v-mid pl3">
+        <div className={styles.divright} style={{textAlign: 'right'}}>
           <h2 className={styles.heading}>Color Palette</h2>
           <h3>Highlighting the best creative solutions</h3>
         </div>
       </section>
     </div>
-    <div className="tc">
-      <img src={Card} />
-    </div>
-    <section className="dt mw6 center pv4 pv5-m pv6-ns">
-      <div className="dtc v-mid">
-        <h2 className={styles.heading}>Font: Quicksand</h2>
-        <h3>Family: Sans Serif</h3>
+   
+      <img src={Card} style={{width: '100%'}} />
+    <div className={styles.container}>
+    <section className={styles.content} style={{marginTop: '4em', marginBottom: '4em'}}>
+      <div className={styles.divleft}>
+        <h2 className={styles.heading}>Quicksand</h2>
+        <h3>Font-Family: Sans Serif</h3>
       </div>
-      <div className="dtc v-mid pl3">
-        <img src={Font} alt="Turquoise Media Fonts Scheme Sofia Moraes copyright" className="mw5" />
+      <div className={styles.divright}>
+        <img src={Font} alt="Turquoise Media Fonts Scheme Sofia Moraes copyright" />
       </div>
     </section>
+    </div>
     <div className="tc">
       <img src={Mobiles} />
+    </div>
+    <div className={styles.container} style={{marginTop: '2em', marginBottom: '4em'}}>
+    <div className={styles.horizontalline} />
     </div>
     <Next link='/quantumpeakcoaching' />
     <Random />
