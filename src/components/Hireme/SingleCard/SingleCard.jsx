434d894cad3412dcb0b3d9dd8756dcc2f8d2fc05
philippzach <mail@philippzach.com>
2018-06-13 22:02:03 +0200

    fixed some console log errors

--- src/components/Hireme/SingleCard/SingleCard.jsx
+++ src/components/Hireme/SingleCard/SingleCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './SingleCard.module.scss';
 
 const SingleCard = props => (
-  <article className="grow pointer br3 mv3" style= {{ boxShadow: "0px 10px 25px 0px rgba(0,0,0,0.20)" }}>
-    <div className="tc pa3 pa4-ns br3 br--top" style= {{ backgroundColor: "#ffdf59"}}>
+  <article className="grow pointer br3 mv3" style={{ boxShadow: '0px 10px 25px 0px rgba(0,0,0,0.20)' }}>
+    <div className="tc pa3 pa4-ns br3 br--top" style={{ backgroundColor: '#ffdf59' }}>
       <img
         src={props.image}
         alt="Sofia Moraes Graphic UI UX Designer"
@@ -12,16 +12,17 @@ const SingleCard = props => (
       />
     </div>
     <div className="tc">
-      <h1 className="f3 mb0"><b>{props.heading}</b></h1>
-      <div >
-      <h2 className="f5 fw4 gray mt0 mb0" style={{fontWeight: '200'}}>{props.heading2}</h2>
-      <div className={styles.linetop}></div>
-      <ul style={{ listStyle: "none", marginLeft: 0, paddingBottom: '2em'}}>
-        {props.list.map(function(listValue){
-          return <li className={styles.items}>{listValue}</li>
-        })}
-      </ul>
-      
+      <h1 className="f3 mb0">
+        <b>{props.heading}</b>
+      </h1>
+      <div>
+        <h2 className="f5 fw4 gray mt0 mb0" style={{ fontWeight: '200' }}>
+          {props.heading2}
+        </h2>
+        <div className={styles.linetop} />
+        <ul style={{ listStyle: 'none', marginLeft: 0, paddingBottom: '2em' }}>
+          {props.list.map(listValue => <li className={styles.items}>{listValue}</li>)}
+        </ul>
       </div>
     </div>
   </article>
