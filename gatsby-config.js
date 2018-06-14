dd60fafd750da22e2240cbf68dcba69e3129b760
philippzach <mail@philippzach.com>
2018-06-14 17:01:17 +0200

    added Do Not Track Option

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: config.googleAnalyticsID,
+        respectDNT: true,
       },
     },
     {
