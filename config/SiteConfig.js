d9da5f2c51d7d221eed28bc8907d70a7f36b39cf
philippzach <mail@philippzach.com>
2018-06-26 15:21:15 +0200

    added somthing

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'Sofia Moraes | Design and Product Photography', // Navigation and Site Title
-  siteTitleAlt: 'Prefessional Graphic and Web Desginer with an eye for photography', // Alternative Site title for SEO
-  siteUrl: 'https://sofiamoreas.com/', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'Professional Graphic and Web Desginer with an eye for photography', // Alternative Site title for SEO
+  siteUrl: 'https://sofiamoraes.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Portfolio of Sofia Moreas, Designer',
-  siteFBAppID: '123456789', // Facebook App ID
-  userTwitter: 'emma', // Twitter Username
+  siteDescription: 'Portfolio of Sofia Moraes',
+  //siteFBAppID: '123456789', // Facebook App ID
+  //userTwitter: 'emma', // Twitter Username
   // og: Meta Tags
-  ogSiteName: 'emma', // Facebook Site Name
+  //ogSiteName: 'emma', // Facebook Site Name
   googleAnalyticsID: 'UA-120790716-1',
-  copyright: 'Copyright © 2018. All rights reserved.', // Copyright in the footer of the site
+  copyright: 'Copyright Philipp Zach © 2018. All rights reserved.', // Copyright in the footer of the site
   // You can translate these three words into your language if you want.
   // They'll be shown on the project page header
   client: 'Client',
